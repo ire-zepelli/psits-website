07fc2b795ca0ecf15ab517516784c90be9becccb
Benedict <benedictavenido1320@gmail.com>
2025-05-26 20:01:46 +0800

    [+] 50% progress for dashboard

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,12 +4,15 @@ const Hero = () => {
   return (
     <div className={styles.hero}>
       <div className={styles.featured}>
+        <div className={styles.gradient}></div>
         <img src="/sample-event-1.png" alt="" />
       </div>
-      <h4>HEADLINES</h4>
-      <h1>
-        <span>CESAFI 2025:</span> Computer Quiz Bowl
-      </h1>
+      <div className={styles["headline-text"]}>
+        <h4>HEADLINES</h4>
+        <h1>
+          <span>CESAFI 2025:</span> Computer Quiz Bowl
+        </h1>
+      </div>
     </div>
   );
 };
